5922551dc397e373fd5aa9e60f9db1b1fee4a761
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2025-10-20 01:04:45 -0400

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/elias-codes-folio/',
   server: {
     host: "::",
     port: 8080,
